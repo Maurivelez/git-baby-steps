f597d39339f97c1c3530b8999c42aca960cdeeac
Mauri Velez <74325182+Maurivelez@users.noreply.github.com>
2021-02-08 12:50:12 -0300

    Add files via upload

---
+++ Concat.js
@@ -0,0 +1,9 @@
+///Concat
+Create a function that concatenates texts.
+
+Implement a function named concat that receives one or more texts (string) as parameters and returns them concatenated in one single string.///
+
+function spell(text1, text2){
+console.log(text1.concat(text1,text2));}
+
+
