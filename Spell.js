151b528b4ef056e7a9a1754aaa6e2c0d2dea4ab1
Mauri Velez <74325182+Maurivelez@users.noreply.github.com>
2021-02-11 18:25:58 -0300

    Update Spell.js

--- Spell.js
+++ Spell.js
@@ -2,6 +2,13 @@
 
 Implement a function named spell that receives a text (string) as a parameter and prints each character from that text to the console.//
 
-function spell(text){
-console.log(text.split(''));
+// function spell(text){
+// console.log(text.split(''));
+// }
+
+function  spell(text) {
+  var caracter = text.split('');  
+  for (var i = 0; i < caracter.length; i++);
+    console.log(caracter[i]);
 }
+spell ("mauri")
